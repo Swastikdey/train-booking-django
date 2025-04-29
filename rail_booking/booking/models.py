8087693946543b527fd5e4ef99c8f219a8f9ba74
Swastik Dey �� <swastik.dey.2003@gmail.com>
2025-04-30 01:19:42 +0530

    Add Total fare before booking

--- rail_booking/booking/models.py
+++ rail_booking/booking/models.py
@@ -32,6 +32,8 @@ class Booking(models.Model):
         MaxValueValidator(10)
     ]
 )
+    #total_fare = models.DecimalField(max_digits=10, decimal_places=2)
+
     status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='CONFIRMED')
 
     def __str__(self):
